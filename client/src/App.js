9334ee8a784f96dadf2801a2a2f645595f721c32
Kumari-Ujwala-Sinha <kumariujwalas@gmail.com>
2021-12-23 12:51:17 +0530

    first commit

---
+++ client/src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import {BrowserRouter as Router} from 'react-router-dom'
+
+
+
+function App() {
+ 
+
+
+
+
+  return (
+    <Router>
+      <div className="App">
+        HelloWorld
+      </div>
+    </Router>
+  );
+}
+
+export default App;
